c4041364d8faaa15cc875ccc1128329bf20a43f9
程梦杰 <18788567655@163.com>
2020-05-20 13:28:53 +0800

    change OnCreate to Init

--- example/src/hello/service/service.go
+++ example/src/hello/service/service.go
@@ -1,9 +1,17 @@
 package service
 
-import ."github.com/CP-Panizza/Panizza"
+import (
+	. "github.com/CP-Panizza/Panizza"
+	"fmt"
+)
 
 type HelloService struct{
 
+}
+
+//在结构体初始化后调用，用于做构造函数使用
+func (this *HelloService)Init(){
+	fmt.Println("init run!")
 }
 
 func (this *HelloService)Hello(ctx *HandleContext){
